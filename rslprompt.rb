8e4e189996d4bff7fce52d55adceae4086bf7298
Rylan Polster <rslpolster@gmail.com>
2026-03-09 10:14:40 +0000

    rslprompt: add 0.1.0 bottle.

--- rslprompt.rb
+++ rslprompt.rb
@@ -5,6 +5,12 @@ class Rslprompt < Formula
   sha256 "086971b97b13c1afe3d807f618d66864e9b9def3adb521ea94189011c53602e2"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/rylan12/personal"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "e638ae9990b7ff4416a95d2a3f1fb03e81ee410c4444daadc34b58e9c4b0a48a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9434645ba4440078351117e6dea6a60047304aec179576e9602c4c5e167fd6f8"
+  end
+
   depends_on "rust" => :build
 
   def install
